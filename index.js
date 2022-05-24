a9fd5d42a5a11cdddadc992844e8f43584c659e3
Radomir Petkov <radomirpetkov@abv.bg>
2022-05-24 10:32:28 +0300

    create home view

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const http = require(`http`)
 const port = 3000
+const homeView = require(`./homeView`)
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
-        'Content-Type': 'text/plain'
+        'Content-Type': 'text/html'
     })
 
-    res.write(`Hello JS WORLD!`)
+    res.write(homeView)
     res.end()
 
 }).listen(port, () => console.log(`Server is listening on port 3000...`))
