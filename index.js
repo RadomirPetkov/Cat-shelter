3b9dd1a02c8883a680b8492052897ee86ec9dac8
Radomir Petkov <radomirpetkov@abv.bg>
2022-05-24 10:52:37 +0300

    add css

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const http = require(`http`)
 const port = 3000
 const homeView = require(`./homeView`)
+const css = require(`./content/styles/site`)
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
+    if (req.url == "/content/styles/site.css") {
+        res.writeHead(200, {
+            'Content-Type': 'text/css'
+        })
+        res.write(css)
+    }
 
-    res.write(homeView)
+    else {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+
+        res.write(homeView)
+    }
     res.end()
 
 }).listen(port, () => console.log(`Server is listening on port 3000...`))
